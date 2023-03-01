39c04e7ab06a72c8ffbf1d7569ab2f9825b68bc6
Rahul K G <kgrahul96@gmail.com>
2023-03-02 02:23:20 +0530

    created api for posting and updating questions and options

--- routes/questions.js
+++ routes/questions.js
@@ -33,7 +33,35 @@ router.post("/createqstns", async (req, res) => {
     });
   }
 });
+//PATCH
+router.patch("/updateqstns", async (req, res) => {
+  try {
+    console.log(req.body);
 
+    const { question1, qstn1option, question2, qstn2option, question3, qstn3option} =
+      req.body;
+   const id = await Question.find().sort({"_id" : -1}).limit(1)
+   if(qstn1option) {
+const data= await Question.findOneAndUpdate( {_id: id}, {$set: { qstn1option:qstn1option}} );
+   }
+   if(qstn2option) {
+    const data= await Question.findOneAndUpdate( {_id: id}, {$set: { qstn2option:qstn2option}} );
+       }
+       if(qstn3option) {
+        const data= await Question.findOneAndUpdate( {_id: id}, {$set: { qstn3option:qstn3option}} );
+           }
+    res.json({
+      status: "success",
+      message: "Questions with options updated successfully",
+      qstn1option, 
+    });
+  } catch (e) {
+    res.status(500).json({
+      status: "Failed",
+      message: e.message,
+    });
+  }
+});
 ///get
 
 router.get("/getqstns", async (req, res) => {
